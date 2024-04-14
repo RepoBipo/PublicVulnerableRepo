f6ccfb49d40b8c7bae34e09b3343487ca08a586c
Matan Shabtay <typhoon.bf2@gmail.com>
2024-04-14 09:05:50 +0300

    another vul

---
+++ VulnerableApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace VulnerableApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var parsedJson = "notJson";
+
+            Console.WriteLine(parsedJson);
+        }
+    }
+}
