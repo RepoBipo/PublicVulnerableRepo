d815582c498eca0fae29015f0e5b9804f19e585b
Matan Shabtay <typhoon.bf2@gmail.com>
2024-04-14 08:29:55 +0300

    vul

---
+++ VulnerableApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace VulnerableApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var highlyNestedPayload = args[0];
+
+            var parsedJson = JsonConvert.DeserializeObject(highlyNestedPayload);
+
+            Console.WriteLine(parsedJson);
+        }
+    }
+}
